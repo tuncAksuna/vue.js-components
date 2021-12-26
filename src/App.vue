51d9ed2c7b1c9c77f81855eeb03a0dd515df1ad7
tuncAksuna <aksuna.tunc@gmail.com>
2021-12-26 22:22:05 +0300

    unnecassary imports deleted

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-    <home-component></home-component>
+  <home-component></home-component>
 </template>
 
 <script>
+
+import home from "./Home.vue";
 export default {
- 
+  components: {
+    Home: home,
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,20 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Home from './Home.vue'
-import Servers from './components/Servers.vue'
-import HomePage from './components/HomePage.vue'
-import Headers from './components/Headers.vue'
-import MainFooter from './components/MainFooter.vue'
-import ServerDetail from './components/ServerDetail.vue'
 
-
-//* importing lamp-component globally
 Vue.component("home-component", Home);
-Vue.component("servers-component", Servers);
-Vue.component("homepage-component", HomePage);
-Vue.component("headers-component", Headers);
-Vue.component("footer-component", MainFooter);
-Vue.component("serverdetail-component", ServerDetail);
 
 new Vue({
     el: '#app',
